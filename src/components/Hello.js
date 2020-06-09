002e63457397d0278f0a7313498980b7a4292053
Le Gossler <ingovg@gmail.com>
2020-06-09 06:31:44 +0200

    First commit of work done so far

---
+++ src/components/Hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hello = () => {
+  return (
+    <div className="defaultName">
+      <h1>Hello Ingo</h1>
+    </div>
+  );
+  //   return React.createElement(
+  //     "div",
+  //     { id: "hello", className: "defaultName" },
+  //     React.createElement("h1", null, "Hello INgo")
+  //   );
+};
+
+export default Hello;
